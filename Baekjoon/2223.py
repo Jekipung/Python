e80f5954f87b802cb853a2a0c80a5387e9c31f0c
한Jekipung국 <94054859+Jekipung@users.noreply.github.com>
2024-04-10 18:18:38 +0900

    Create 2223.py

---
+++ Baekjoon/2223.py
@@ -0,0 +1,21 @@
+import sys; inpit = sys.stdin.readline
+
+t, x, m = map(int, input().split())
+
+if m == 0:
+    print(t * x)
+    exit()
+
+min_x = float('inf')
+
+for _ in range(m):
+    d, s = map(int, input().strip().split())
+    if min_x > (d-1)//s:
+        min_x = (d-1)//s
+
+if min_x == 0:
+    print(0)
+elif t > min_x:
+    print((min_x + ((t - min_x)//2)) * x)
+else:
+    print(t * x)
